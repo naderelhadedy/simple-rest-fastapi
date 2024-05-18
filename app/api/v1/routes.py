e7aca94ed1953c64d883c077a53713f10d084250
naderelhadedy <elhadedynader@gmail.com>
2024-05-18 17:55:38 +0300

    chore: disabled tag option in swagger

--- app/api/v1/routes.py
+++ app/api/v1/routes.py
@@ -10,5 +10,5 @@ routers = APIRouter()
 router_list = [person_router]
 
 for router in router_list:
-    router.tags = routers.tags.append("v1")
+    # router.tags = routers.tags.append("v1")
     routers.include_router(router)
